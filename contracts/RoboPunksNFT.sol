411088ca7e79ba48c10c5a7964c47df27639cc49
Danylo <danillazorenko12@gmail.com>
2023-10-10 19:41:31 +0300

    init

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-import import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract RoboPunksNFT is ERC721, Ownable {
@@ -38,5 +38,18 @@ contract RoboPunksNFT is ERC721, Ownable {
     function withdraw() external onlyOwner {
         (bool success, ) = withdrawWallet.call{ value: address(this).balance}('');
         require(success, 'withdraw failed');
+    }
+
+    function mint(uint256 quantity_) public payable {
+        require(isPublicMintEnabled, 'minting not enabled');
+        require(msg.value == quantity_ * mintPrice. 'wrong mint value');
+        require(totalSupply + quantity_ <= maxSupply, 'sold out');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+        for (uint256 i = 0; i < quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
     }
 }
